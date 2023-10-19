401bdb7a4a01d0738ab9fd01213d4564b56c951e
Olczak_Mikolaj <01159104@pw.edu.pl>
2023-10-19 21:57:08 +0200

    adding simple home hook

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!').end();
+app.use(express.json())
+
+app.get('/home', (req, res) => {
+  res.status(200).send({id:42069, username: 'Mikołaj', steps: 2000, maxsteps: 20000, achievements: {calories: 630, distance: 9600, water: 1200, basketball: 92, soccer: 26, squats: 42}, stats:{calories: 1513, maxcalories: 2300, carbs: 150, maxcarbs: '263-410', protein: 60, maxprotein: '70-117', fats: 55, maxfats: '65-78'}}).end();
 });
 
 const PORT = parseInt(process.env.PORT) || 8080;
